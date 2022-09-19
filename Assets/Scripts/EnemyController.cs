0ffe4322a1879cd0eb15e38f37cca75c20846e53
mx9721 <78099862+mx9721@users.noreply.github.com>
2022-09-19 12:49:47 -0400

    enemy AI start

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -13,6 +13,17 @@ public class EnemyController : MonoBehaviour {
 	[SerializeField] private Transform target;
 	[SerializeField] private float shootTime;
 
+	protected Vector3 acceleration;
+	protected Vector3 velocity;
+	protected Vector3 desiredVelocity;
+	protected Vector3 steeringForce;
+
+	protected float maxSpeed;
+	protected float maxForce;
+	protected float mass = 1.0f;
+	protected float rad;
+	protected float separationDistance = 10.0f;
+
 	private float timer;
 
 	private void Start ( ) {
@@ -28,6 +39,18 @@ public class EnemyController : MonoBehaviour {
 				BulletController.SpawnBullet(bulletPrefab, transform.position, (target.position - transform.position).normalized, BulletType.ENEMY);
 				timer = shootTime;
 			}
+
+			ApplyForce(acceleration);
+
+			velocity += acceleration * Time.deltaTime;
+
+			velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+			transform.position += velocity;
+
+			acceleration = Vector3.zero;
+
+			transform.forward = velocity.normalized;
 		}
 	}
 
@@ -35,6 +58,72 @@ public class EnemyController : MonoBehaviour {
 	// int damage: The amount of health to make the player lose
 	public void TakeDamage (int damage) {
 		// TO DO: Implement this later
+	}
+
+	// Update the acceleration of the enemy using all the different forces
+	public void ApplyForce(Vector3 force)
+	{
+		acceleration += force / mass;
+	}
+
+	public Vector3 CalculateFuturePosition(float futureTime)
+	{
+		Vector3 futurePosition = transform.position + velocity * futureTime;
+
+		return futurePosition;
+	}
+
+	public Vector3 StayInBounds(Vector3 center)
+	{
+		Vector3 returnForce = Vector3.zero;
+
+		if (transform.position.x > 74 || transform.position.x < -74 || transform.position.z > 74 || transform.position.z < -74)
+		{
+			returnForce = Seek(center) * 250;
+
+			return returnForce;
+		}
+		else if (transform.position.x > 60 || transform.position.x < -60 || transform.position.z > 60 || transform.position.z < -60)
+		{
+			returnForce = Seek(center);
+
+			return returnForce;
+		}
+		else
+		{
+			return Vector3.zero;
+		}
+	}
+
+	public Vector3 Wander(float futuretime, float radius)
+	{
+		Vector3 futurePos = CalculateFuturePosition(futuretime);
+
+		float angle = Random.Range(0, 360);
+		float x = Mathf.Cos(angle * Mathf.Deg2Rad) * radius + futurePos.x;
+		float z = Mathf.Sin(angle * Mathf.Deg2Rad) * radius + futurePos.z;
+
+		return (Seek(new Vector3(x, 0, z)));
+	}
+
+	protected Vector3 Seek(Vector3 targetPos)
+	{
+		desiredVelocity = targetPos - transform.position;
+
+		desiredVelocity = desiredVelocity.normalized * maxSpeed;
+
+		steeringForce = desiredVelocity - velocity;
+
+		return steeringForce;
+	}
+
+	public Vector3 Flee(Vector3 targetPos)
+	{
+		desiredVelocity = Seek(targetPos);
+
+		desiredVelocity *= -1;
+
+		return desiredVelocity;
 	}
 }
 
