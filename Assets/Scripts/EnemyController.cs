8162618d8b2007011adcf82c481818754420c674
mx9721 <78099862+mx9721@users.noreply.github.com>
2022-10-03 12:18:21 -0400

    AI update

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -17,13 +17,10 @@ public class EnemyController : MonoBehaviour {
 	[Space]
 	[SerializeField] public Vector2 Movement;
 
-	/*
 	protected Vector2 acceleration = Vector2.zero;
-	protected Vector2 velocity = Vector2.zero;
 	protected Vector2 desiredVelocity = Vector2.zero;
 	protected Vector2 steeringForce = Vector2.zero;
 	protected Vector2 ultimateForce = Vector2.zero;
-	*/
 
 	private float timer;
 
@@ -66,23 +63,18 @@ public class EnemyController : MonoBehaviour {
 
 	private void FixedUpdate ( ) {
 		// Either seek or wander around depending on if the player is not null
-		/*
 		if (player != null) {
 			ultimateForce += Seek(player.position);
 		} else {
 			ultimateForce += Wander(1f, 3f);
 		}
-		*/
+		
+		ApplyForce(ultimateForce);
 
-		// ApplyForce(ultimateForce);
+		rigidbody2D.velocity += Time.fixedDeltaTime * acceleration;
+		rigidbody2D.velocity = Vector2.ClampMagnitude(rigidbody2D.velocity, moveSpeed);
 
-		// Move the enemy
-		// velocity += acceleration * Time.deltaTime;
-		// velocity = Vector3.ClampMagnitude(velocity, moveSpeed);
-
-		rigidbody2D.velocity = moveSpeed * Time.fixedDeltaTime * Movement;
-
-		// acceleration = Vector3.zero;
+		acceleration = Vector2.zero;
 	}
 
 	// Have the player lose health
@@ -93,7 +85,7 @@ public class EnemyController : MonoBehaviour {
 		// Introduce a shine() method to show when the enemy takes damage
 	}
 
-	/*
+	
 	// Apply the direction the enemy is going to calculate the acceleration towards that direction
 	public void ApplyForce (Vector2 force) {
 		acceleration += force;
@@ -105,45 +97,44 @@ public class EnemyController : MonoBehaviour {
 	// Point the enemy towards a target position
 	protected Vector2 Seek (Vector2 targetPos) {
 		//Find the direction for the enemy to point towards
-		desiredVelocity = targetPos - (Vector2) transform.position;
+		desiredVelocity = targetPos - (Vector2)transform.position;
 
 		//Normalize the vector
 		desiredVelocity = desiredVelocity.normalized * moveSpeed;
 
 		//Calculate the vector for the enemy to steer towards
-		steeringForce = desiredVelocity - velocity;
+		steeringForce = desiredVelocity - rigidbody2D.velocity;
 
 		return steeringForce;
 	}
-		// Run away from the target position
-		public Vector2 Flee (Vector2 targetPos) {
-			desiredVelocity = -Seek(targetPos);
 
-			desiredVelocity *= -1;
+	// Run away from the target position
+	public Vector2 Flee (Vector2 targetPos) {
+		desiredVelocity = -Seek(targetPos);
 
-			return desiredVelocity;
-		}
+		desiredVelocity *= -1;
 
-		// Used to calculate the future position of the enemy
-		public Vector2 CalculateFuturePosition (float futureTime) {
-			return (Vector2) transform.position + (velocity * futureTime);
-		}
+		return desiredVelocity;
+	}
 
-		// Wander around the arena to simulate enemy movement
-		public Vector2 Wander (float futuretime, float radius) {
-			// Calculate the future position of the enemy
-			Vector2 futurePos = CalculateFuturePosition(futuretime);
+	// Used to calculate the future position of the enemy
+	public Vector2 CalculateFuturePosition (float futureTime) {
+		return (Vector2) transform.position + (rigidbody2D.velocity * futureTime);
+	}
 
-			// Go in a random angle in a radius from the calculated position
-			float angle = Random.Range(0, 360);
-			float x = Mathf.Cos(angle * Mathf.Deg2Rad) * radius + futurePos.x;
-			float y = Mathf.Sin(angle * Mathf.Deg2Rad) * radius + futurePos.y;
+	// Wander around the arena to simulate enemy movement
+	public Vector2 Wander (float futuretime, float radius) {
+		// Calculate the future position of the enemy
+		Vector2 futurePos = CalculateFuturePosition(futuretime);
 
-			return (Seek(new Vector2(x, y)));
-		}
-	*/
+		// Go in a random angle in a radius from the calculated position
+		float angle = Random.Range(0, 360);
+		float x = Mathf.Cos(angle * Mathf.Deg2Rad) * radius + futurePos.x;
+		float y = Mathf.Sin(angle * Mathf.Deg2Rad) * radius + futurePos.y;
 
-
+		return (Seek(new Vector2(x, y)));
+	}
+	
 	// Fires multiple bullets at once
 	// int numBullets: The number of bullets to fire
 	// float angleSpread: The cone that all the bullets will be encompassed in when firing
