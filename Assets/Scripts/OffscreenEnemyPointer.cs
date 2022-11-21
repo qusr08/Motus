9bcdfafef32c611d832e6138223960bd6da389fa
sf6168 <sf6168@rit.edu>
2022-11-20 21:58:45 -0500

    Please test; offscreen enemy pointer implemented
    Not functioning but I don't know why.

--- Assets/Scripts/OffscreenEnemyPointer.cs
+++ Assets/Scripts/OffscreenEnemyPointer.cs
@@ -5,20 +5,42 @@ using UnityEngine;
 public class OffscreenEnemyPointer : MonoBehaviour
 {
     //should only be instantiated when an enemy is instantiated
-    //Enemy field
-    //Camera field
+    public GameObject enemyPointer;
+    public GameObject target;
+    Renderer renderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        //bind to enemy
-            //change color to enemy's color
-        //bind to camera
+        renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!renderer.isVisible)
+        {
+            if(!enemyPointer.activeSelf)
+            {
+                enemyPointer.SetActive(true);
+            }
+
+            Vector2 direction = target.transform.position - transform.position;
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, direction);
+
+            if(ray.collider != null)
+            {
+                enemyPointer.transform.position = ray.point;
+            }
+        }
+        else
+        {
+            if (enemyPointer.activeSelf)
+            {
+                enemyPointer.SetActive(false);
+            }
+
+        }
         //show only if bound enemy is not being rendered by camera
         //update position relative to enemy and center of camera; orbit should not overlap with bars on the top-left
         //destroy with enemy
