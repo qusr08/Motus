152343774f09602a8eed81440ef7546aecb10c8b
sf6168 <sf6168@rit.edu>
2022-11-20 21:14:47 -0500

    updated offscreen enemy pointer

--- Assets/Scripts/OffscreenEnemyPointer.cs
+++ Assets/Scripts/OffscreenEnemyPointer.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class OffscreenEnemyPointer : MonoBehaviour
 {
+    //should only be instantiated when an enemy is instantiated
+    //Enemy field
+    //Camera field
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //bind to enemy
+            //change color to enemy's color
+        //bind to camera
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //show only if bound enemy is not being rendered by camera
+        //update position relative to enemy and center of camera; orbit should not overlap with bars on the top-left
+        //destroy with enemy
     }
 }
